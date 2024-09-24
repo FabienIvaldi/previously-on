3f6bf597d2a07147d80a09212f0f4d491cc3f262
Fabien ivaldi <fabien.ivaldi@epitech.eu>
2024-09-24 20:27:03 +0200

    commit

---
+++ front/src/app/app.routes.ts
@@ -0,0 +1,20 @@
+import { RouterModule, Routes } from "@angular/router";
+import { LoginComponent } from "./components/login/login.component";
+import { SeriesListComponent } from './components/series-list/series-list.component';
+import { ProfileComponent } from "./components/profile/profile.component";
+import { NgModule } from "@angular/core";
+import { AuthGuard } from "./auth.guard";
+import { MovieDetailComponent } from "./components/movie-detail/movie-detail.component";
+
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'list', component: SeriesListComponent },
+    { path: 'movie/:id', component: MovieDetailComponent },
+    { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
+    { path: '**', redirectTo: '/login', pathMatch: 'full' },
+];
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
+})
+export class AppRoutingModule { }
